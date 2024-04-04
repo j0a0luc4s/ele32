e5acb8dc335577fcb6ca591985d6c766f9c65872
joao <joaolucasrrolim@gmail.com>
2024-04-04 16:26:43 -0300

    Plotting settinggs

--- test/1/runtests.jl
+++ test/1/runtests.jl
@@ -1,12 +1,15 @@
 using Random
 using Plots
+using LaTeXStrings
 
 @testset "1" begin
     pv = Float64[]
     hammingv = Float64[]
     matrixv = Float64[]
+    nocodev = Float64[]
 
-    for p = [0.5, 0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.002, 0.001, 0.0005, 0.0002, 0.0001]
+    p = 0.5
+    while p > 1e-2
         @testset "p = $p" begin
             original = bitrand(1000008)
 
@@ -18,20 +21,42 @@ using Plots
             matrix = ele32.Channels.binarySymmetric(matrix, p)
             matrix = ele32.MatrixCodes.decodeMany(matrix)
 
+            nocode = ele32.Channels.binarySymmetric(original, p)
+
             wrong_hamming = count(.==(false), hamming .== original)
             wrong_matrix = count(.==(false), matrix .== original)
+            wrong_nocode = count(.==(false), nocode .== original)
             total = length(original)
 
             push!(pv, p)
             push!(hammingv, wrong_hamming/total)
             push!(matrixv, wrong_matrix/total)
+            push!(nocodev, wrong_nocode/total)
 
             println("p = $p")
             println("pb_hamming = ", wrong_hamming, "/", total, " = ", hammingv[end])
             println("pb_matrix = ", wrong_matrix, "/", total, " = ", matrixv[end])
+            println("pb_nocode = ", wrong_nocode, "/", total, " = ", nocodev[end])
 
-            plt = plot(pv, [hammingv matrixv], yscale=:log10)
+            plt = plot(
+                pv,
+                [hammingv matrixv nocodev],
+                xscale=:log10,
+                yscale=:log10,
+                xticks=[],
+                yticks=[],
+                xflip=true,
+                xlabel="\$p\$",
+                ylabel="\$P_b\$",
+                label=["Hamming" "Matrix" "Nocode"],
+                legend=:bottomleft,
+                left_margin=5Plots.mm,
+                dpi=400
+            )
             display(plt)
+            #savefig("plot.png")
+
+            p *= 0.9
         end
     end
 end
